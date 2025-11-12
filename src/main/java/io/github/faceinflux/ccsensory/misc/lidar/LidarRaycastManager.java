b223c7ecf99a8248fee8e437938dded1c898ad50
FaceInFlux <FaceInFlux@gmail.com>
2025-11-12 11:24:38 -0800

    Remove unused things from LidarRaycastManager

--- src/main/java/io/github/faceinflux/ccsensory/misc/lidar/LidarRaycastManager.java
+++ src/main/java/io/github/faceinflux/ccsensory/misc/lidar/LidarRaycastManager.java
@@ -1,18 +1,9 @@
 package io.github.faceinflux.ccsensory.misc.lidar;
 
-import net.minecraft.util.Tuple;
-import org.jspecify.annotations.Nullable;
-
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Queue;
-import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public final class LidarRaycastManager {
     private static final LinkedBlockingDeque<LidarScanRequest> REQUESTS = new LinkedBlockingDeque<>();
-    private static final ArrayList<Integer> IDS = new ArrayList<>();
     private static final LidarRaycastThread THREAD = new LidarRaycastThread();
 
     static {
